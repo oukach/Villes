cbb9ba45e71ab8f44a35dd2e03c48582f0187d5c
AOUKACH EL MEHDI <54449772+oukach@users.noreply.github.com>
2022-01-08 19:15:00 +0100

    Add files via upload

--- src/ExerciceNio/Main.java
+++ src/ExerciceNio/Main.java
@@ -1,18 +1,12 @@
 package ExerciceNio;
-
-import ExerciceNio.metier.IServiceVille;
-import ExerciceNio.metier.ServiceVilles;
-
+import ExerciceNio.Vue.CityMainFrame;
 import java.io.*;
-import java.nio.file.Paths;
 
 /**
- * @author <h2 style="color:red;">AOUKACH EL MEHDI, OUSSAMA ER-RZIKI</h2>
+ * @author <h2 style="color:red;">AOUKACH EL MEHDI</h2>
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-        IServiceVille service = new ServiceVilles(Paths.get("villes.txt"));
-        service.lireBDVilles_NIO().forEach(System.out::println);
-        service.trierVilles(service.lireBDVilles_NIO());
+        new CityMainFrame("Dashboard");
     }
 }
